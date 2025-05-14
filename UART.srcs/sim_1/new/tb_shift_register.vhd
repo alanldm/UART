58566ff55a3c03d99809aee33956e5f796450110
Alan Lima <alan.lima@ganil.com>
2025-05-14 10:07:30 +0200

    MVP1/UART - Set up Vivado project and .gitignore

---
+++ UART.srcs/sim_1/new/tb_shift_register.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 14.05.2025 09:50:44
+-- Design Name: 
+-- Module Name: tb_shift_register - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity tb_shift_register is
+--  Port ( );
+end tb_shift_register;
+
+architecture Behavioral of tb_shift_register is
+
+begin
+
+
+end Behavioral;
