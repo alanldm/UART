13a9946087f1e81732071f454881f8df402075bf
Alan Lima <alan.lima@ganil.com>
2025-05-14 12:28:02 +0200

    MVP1/UART - Shift register : Simulation for the shift register

--- UART.srcs/sim_1/new/tb_shift_register.vhd
+++ UART.srcs/sim_1/new/tb_shift_register.vhd
@@ -1,43 +1,72 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 14.05.2025 09:50:44
--- Design Name: 
--- Module Name: tb_shift_register - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity tb_shift_register is
 --  Port ( );
 end tb_shift_register;
 
 architecture Behavioral of tb_shift_register is
+    signal data_s : std_logic_vector(7 downto 0) := (others => '0');
+    signal l_s_s : std_logic := '0';
+    signal enable_s : std_logic := '0';
+    signal rst_s : std_logic := '0';
+    signal clk_s : std_logic := '0';
+    signal lsb_s : std_logic := '0';
+    
+    component shift_register is
+        Port (
+            data : in std_logic_vector(7 downto 0);
+            l_s : in std_logic;
+            enable : in std_logic;
+            rst : in std_logic;
+            clk : in std_logic;
+            lsb : out std_logic
+        );
+    end component;
 
-begin
-
-
+    begin
+        inst_shift_register : shift_register port map (
+            data => data_s,
+            l_s => l_s_s,
+            enable => enable_s,
+            rst => rst_s,
+            clk => clk_s,
+            lsb => lsb_s 
+        );
+        
+        proc_clk_s : process
+            begin
+                clk_s <= not clk_s;
+                wait for 10 ns;
+        end process;
+        
+        proc_signals : process
+            begin
+                data_s <= "10010011";
+                l_s_s <= '1';
+                enable_s <= '1';
+                rst_s <= '0';
+                
+                wait for 25 ns;
+                data_s <= "10010011";
+                l_s_s <= '0';
+                enable_s <= '1';
+                rst_s <= '1';
+                
+                wait for 10 ns;
+                data_s <= "10010011";
+                l_s_s <= '1';
+                enable_s <= '1';
+                rst_s <= '0';
+                
+                wait for 10 ns;
+                data_s <= "10010011";
+                l_s_s <= '0';
+                enable_s <= '1';
+                rst_s <= '0';
+                
+                wait for 90 ns;
+                
+                wait;
+        end process;
 end Behavioral;
