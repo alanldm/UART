ffa5036f79ba0956a6bcb7b73da39514fcb0be25
Alan Lima <alan.lima@ganil.com>
2025-05-14 11:19:33 +0200

    MVP1/UART - Shift register : Behavioral of a shift register

--- UART.srcs/sources_1/new/shift_register.vhd
+++ UART.srcs/sources_1/new/shift_register.vhd
@@ -1,43 +1,39 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 14.05.2025 09:49:39
--- Design Name: 
--- Module Name: shift_register - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity shift_register is
---  Port ( );
+    Port (
+        data : in std_logic_vector(7 downto 0);
+        l_s : in std_logic;
+        enable : in std_logic;
+        rst : in std_logic;
+        clk : in std_logic;
+        lsb : out std_logic
+    );
 end shift_register;
 
-architecture Behavioral of shift_register is
+architecture Behavioral of shift_register is    
+    signal shift_reg : std_logic_vector(7 downto 0) := (others => '0');
+    
+    begin 
 
-begin
-
+    process(clk, rst)
+        begin
+            if (rst = '1') then
+                shift_reg <= (others => '0');
+            else
+                if (rising_edge(clk)) then
+                    if (enable = '1') then
+                        if (l_s = '1') then
+                            shift_reg <= data;
+                        else
+                            shift_reg <= '0' & shift_reg(7 downto 1);
+                        end if;
+                    end if;
+                end if;
+            end if;
+    end process;
+    
+    lsb <= shift_reg(0);
 
 end Behavioral;
